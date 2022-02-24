f6494b5d9568122e20cb4d385f8b95b6eb568791
EddieLo <eddarlostudy@yahoo.com>
2022-02-24 03:43:09 -0800

    getting answers to quiz

--- assets/js/highscores.js
+++ assets/js/highscores.js
@@ -5,7 +5,7 @@ var initialsObj = [];
 var scoreObj = [];
 
 // retrieving information from local storage
-if(!(localStorage.getItem("initials") === null)) {
+if (!(localStorage.getItem("initials") === null)) {
     // using json parse to change from a string to an object
     initialsObj = JSON.parse(localStorage.getItem("initials"));
     scoreObj = JSON.parse(localStorage.getItem("score"));
@@ -13,21 +13,17 @@ if(!(localStorage.getItem("initials") === null)) {
     // creating information given by the user
     for (var i = 0; i < initialsObj.length; i++) {
         var scoreEl = document.createElement("p");
-        scoreEl.setAttribute("class","scores");
-        scoreEl.textContent = (i+1) + ". " + initialsObj[i] + " - " + scoreObj[i];
+        scoreEl.setAttribute("class", "scores");
+        scoreEl.textContent = i + 1 + ". " + initialsObj[i] + " - " + scoreObj[i];
 
         highscoresEl.appendChild(scoreEl);
     }
-    
-
-}
-else {
+} else {
     console.log("error");
 }
 // used to remove any user datas stored
-clearBtn.addEventListener("click", function() {
+clearBtn.addEventListener("click", function () {
     highscoresEl.textContent = "";
     localStorage.removeItem("initials");
     localStorage.removeItem("score");
-
 });
--- assets/js/submit.js
+++ assets/js/submit.js
@@ -1,33 +1,27 @@
 var timer = document.querySelector(".clock");
 var userFormEl = document.querySelector(".highscores");
-var nameInputEl = document.querySelector(".form")
-
+var nameInputEl = document.querySelector(".form");
 var queryString = document.location.search;
 var score = queryString.split("=")[1];
 timer.textContent = "Score: " + score;
 var initialsObj = [];
 var scoreObj = [];
+// form used to set user initials into local storage
+userFormEl.addEventListener("submit", function (event) {
+  event.preventDefault();
+  var initials = nameInputEl.value.trim();
 
+  if (!(localStorage.getItem("initials") === null)) {
+    initialsObj = JSON.parse(localStorage.getItem("initials"));
+    scoreObj = JSON.parse(localStorage.getItem("score"));
+    initialsObj.push(initials);
+    scoreObj.push(score);
+  } else {
+    initialsObj.push(typeof initialsObj);
+    scoreObj.push(score);
+  }
 
-
-userFormEl.addEventListener("submit", function(event) {
-    event.preventDefault();
-    var initials = nameInputEl.value.trim();
-    
-    if(!(localStorage.getItem("initials") === null)){
-        initialsObj = JSON.parse(localStorage.getItem("initials"));
-        scoreObj = JSON.parse(localStorage.getItem("score"));
-        initialsObj.push(initials);
-        scoreObj.push(score);
- 
-    }
-    else {
-        initialsObj.push(typeof(initialsObj));
-        scoreObj.push(score);
-    }
-
-    localStorage.setItem("initials", JSON.stringify(initialsObj));
-    localStorage.setItem("score", JSON.stringify(scoreObj));
-    location.href = "highscores.html";
-
+  localStorage.setItem("initials", JSON.stringify(initialsObj));
+  localStorage.setItem("score", JSON.stringify(scoreObj));
+  location.href = "highscores.html";
 });
